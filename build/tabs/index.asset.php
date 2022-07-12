a04583b82b444c90443a457098b12c0be0e3d6b8
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2022-07-12 20:55:40 +0600

    Updated scss file

--- build/tabs/index.asset.php
+++ build/tabs/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e4b6657ca3b190bbeee4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9401389b2c3001f9efce');
